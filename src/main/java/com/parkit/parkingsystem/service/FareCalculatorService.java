8f7acf777d2ff223e5d23e7a69717fb835ddace2
Pascal Fares <pascal.fares@cofares.net>
2022-01-27 08:38:25 +0200

    Correction des 3 premiers bug!

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -10,19 +10,28 @@ public class FareCalculatorService {
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
 
-        int inHour = ticket.getInTime().getHours();
-        int outHour = ticket.getOutTime().getHours();
+        //GetTime des milliseconde; /1000 des secondes; /60 des minutes
+        long inMinutes = ticket.getInTime().getTime()/1000/60;
+        long outMinutes = ticket.getOutTime().getTime()/1000/60;
 
         //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
-
+        long durationm = outMinutes - inMinutes;
+        double rate = 1.0;
+        int duration = (int) durationm / 60; 
+        if (durationm < 15) {
+            rate = 0.0;
+        } else if (durationm < 60) {
+            rate = 0.75;
+            duration = 1;
+        }
+       
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
-                ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
+                ticket.setPrice(rate * duration * Fare.CAR_RATE_PER_HOUR);
                 break;
             }
             case BIKE: {
-                ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
+                ticket.setPrice(rate * duration * Fare.BIKE_RATE_PER_HOUR);
                 break;
             }
             default: throw new IllegalArgumentException("Unkown Parking Type");
